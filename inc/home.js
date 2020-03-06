2bc29203e483ef0edbab720ef2693c0089246651
Felipe Riboldi <feliperiboldimarques@gmail.com>
2020-03-06 14:00:54 -0300

    Frontend: Texto do Botão do Banner dinâmico

--- inc/home.js
+++ inc/home.js
@@ -19,7 +19,7 @@ module.exports = {
     getData() {
         return new Promise((resolve, reject) => {
             conn.query(`
-            SELECT title AS bTitle, subtitle AS bSubtitle, href AS bHref, photo AS bPhoto FROM tb_banners ORDER BY title;
+            SELECT title AS bTitle, subtitle AS bSubtitle, href AS bHref, textButton AS bTextButton, photo AS bPhoto FROM tb_banners ORDER BY title;
 
             SELECT id AS nId, title AS nTitle, subtitle AS nSubtitle, photo AS nPhoto FROM tb_news ORDER BY register DESC;
             `, [1, 2], (err, results) => {
